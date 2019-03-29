4ef91c3effc88ef16df6e729aff4d34b0dcfb082
danic96 <dani@example.com>
2019-03-29 10:45:40 +0100

    more database

--- StorageAndGo/models.py
+++ StorageAndGo/models.py
@@ -2,13 +2,19 @@ from django.db import models
 
 # Create your models here.
 
+PRIORITY_TYPE = (
+('A', 'Alta'),
+('S', 'Sense Prioritat'),
+)
+
 
 class TaskMaintenance(models.Model):
     # HI HA TRES PRIORITATS
-    priority = models.IntegerField(choices=)
+    priority = models.CharField('Priority', max_length=1, choices=PRIORITY_TYPE, default='A')
     room = models.ForeignKey("Room", default=1, on_delete=models.PROTECT)
     temperature = models.IntegerField(default=0)
     description = models.TextField(default="")
+    user = models.ForeignKey("User", default="", on_delete=models.PROTECT, related_name="user")
 
     def __unicode__(self):
         # return u"%d - %d - %s" % self.room, self.temperature, self.description
@@ -19,20 +25,22 @@ class TaskOperator(models.Model):
     product = models.TextField(default="")
     origin = models.ForeignKey("Room", default=1, on_delete=models.PROTECT, related_name="origin")
     destination = models.ForeignKey("Room", default=1, on_delete=models.PROTECT, related_name="destination")
-    quantity = models.ManyToManyField("Container", default="")
+    quantity = models.ManyToManyField("Contenidor", default="")
     description = models.TextField()
+    priority = models.CharField('Priority', max_length=1, choices=PRIORITY_TYPE, default='A')
+    user = models.ForeignKey("User", default="", on_delete=models.PROTECT, related_name="user")
 
     def __unicode__(self):
         # return u"%d - %d - %s" % self.room, self.temperature, self.description
-        return u"%s" % self.room
+        return u"%s" % self.description
 
 
-class Container(models.Model):
+class Contenidor(models.Model):
     description = models.TextField(default="")
 
     def __unicode__(self):
         # return u"%d - %d - %s" % self.room, self.temperature, self.description
-        return u"%s" % self.room
+        return u"%s" % self.description
 
 
 class Room(models.Model):
@@ -40,4 +48,4 @@ class Room(models.Model):
 
     def __unicode__(self):
         # return u"%d - %d - %s" % self.room, self.temperature, self.description
-        return u"%s" % self.room
+        return u"%s" % self.description
