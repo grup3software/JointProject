adb4b136a9927c0a17b1455f576950809312230d
danic96 <dani@example.com>
2019-04-09 20:17:53 +0200

    models solved

--- StorageAndGo/models.py
+++ StorageAndGo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import date
 
 # Create your models here.
 
@@ -11,6 +12,7 @@ class Task(models.Model):
     description = models.TextField(default="")
     user = models.ForeignKey(User, on_delete=models.PROTECT, related_name="user_maintenance", blank=True, null=True)
     accepted = models.BooleanField(default=False)
+    priority = models.CharField('Priority', max_length=1, choices=PRIORITY_TYPE, blank=True, null=True)
 
     def get_class(self):
         # return self.__class__.__name__
@@ -19,7 +21,6 @@ class Task(models.Model):
 
 class TaskMaintenance(Task):
     # HI HA DOS PRIORITATS (AMB I SENSE)
-    priority = models.CharField('Priority', max_length=1, choices=PRIORITY_TYPE, blank=True, null=True)
     room = models.ForeignKey("Room", default=1, on_delete=models.PROTECT, blank=True, null=True)
     temperature = models.IntegerField(default=0, blank=True)
 
@@ -36,7 +37,6 @@ class TaskOperator(Task):
     origin = models.ForeignKey("Room", default=1, on_delete=models.PROTECT, related_name="origin", blank=True, null=True)
     destination = models.ForeignKey("Room", default=1, on_delete=models.PROTECT, related_name="destination", blank=True, null=True)
     quantity = models.ManyToManyField("Contenidor", default="", blank=True)
-    priority = models.CharField('Priority', max_length=1, choices=PRIORITY_TYPE, blank=True)
 
     def __unicode__(self):
         # return u"%d - %d - %s" % self.room, self.temperature, self.description
@@ -48,9 +48,9 @@ class TaskOperator(Task):
 
 class Manifesto(models.Model):
     reference = models.BigIntegerField(primary_key=True, default=00000000000)
-    entry_date = models.DateField(auto_now=True)
+    entry_date = models.DateField(default=date.today)
     origin = models.CharField(max_length=255)
-    destination= models.CharField(max_length=255)
+    destination = models.CharField(max_length=255)
 
     def get_length(self):
         return Contenidor.objects.filter(Contenidor.manifesto == self.reference).count()
@@ -63,16 +63,16 @@ class Manifesto(models.Model):
 
 
 class Contenidor(models.Model):
-    name = models.CharField(max_length=200)
-    temp_min = models.SmallIntegerField()
-    temp_max = models.SmallIntegerField()
-    moistness_min = models.PositiveSmallIntegerField()
-    moistness_max = models.PositiveSmallIntegerField()
-    limit_date = models.DateField(default="")
-    manifesto = models.ForeignKey(Manifesto, on_delete= models.CASCADE)
+    name = models.CharField(max_length=200, null=True)
+    temp_min = models.SmallIntegerField(null=True)
+    temp_max = models.SmallIntegerField(null=True)
+    moistness_min = models.PositiveSmallIntegerField(null=True)
+    moistness_max = models.PositiveSmallIntegerField(null=True)
+    limit_date = models.DateField(default=None, null=True)
+    manifesto = models.ForeignKey(Manifesto, on_delete=models.CASCADE, null=True)
 
-    def create_container(self,name,temp_min,temp_max,moistness_min,limit_date,manifesto):
-        self.objects.create(name=name,temp_min=temp_min,temp_max=temp_max,moistness_min=moistness_min,limit_date=limit_date,manifesto=manifesto)
+    def create_container(self, name, temp_min, temp_max, moistness_min, limit_date, manifesto):
+        self.objects.create(name=name, temp_min=temp_min, temp_max=temp_max, moistness_min=moistness_min, limit_date=limit_date, manifesto=manifesto)
 
     def delete_container(self):
         Contenidor.objects.filter(self.id).delete()
